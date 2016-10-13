330f9cd5319fc396087c89715ae79ba371aa5801
Dana Kulp <dkulp23@gmail.com>
2016-10-13 13:42:17 -0700

    Created function to populate an array with the names of the products by using String.slice method.

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var imgPaths = [
+  'img/bag.jpg',
+  'img/banana.jpg',
+  'img/bathroom.jpg',
+  'img/boots.jpg',
+  'img/breakfast.jpg',
+  'img/bubblegum.jpg',
+  'img/chair.jpg',
+  'img/cthulhu.jpg',
+  'img/dog-duck.jpg',
+  'img/dragon.jpg',
+  'img/pen.jpg',
+  'img/pet-sweep.jpg',
+  'img/scissors.jpg',
+  'img/shark.jpg',
+  'img/sweep.jpg',
+  'img/tauntaun.jpg',
+  'img/unicorn.jpg',
+  'img/breakfast.jpg',
+  'img/usb.jpg',
+  'img/water-can.jpg',
+  'img/wine-glass.jpg'
+];
+
+var productNames = [];
+
+var allProducts = [];
+
+function extractNames() {
+  for (var i = 0; i < imgPaths.length; i++) {
+    productNames.push(imgPaths[i].slice(4, -4));
+  }
+}
+
+extractNames();
+
+var Product = function() {
+  this.name = null;
+  this.filePath = thing;
+  this.numTimesShown = 0;
+  this.numTimesClicked = 0;
+  allProducts.push(this);
+}
