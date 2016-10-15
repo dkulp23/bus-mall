c6f72bb529d6f0992c86b4a52c33fa59bdcbd34f
Dana Kulp <dkulp23@gmail.com>
2016-10-15 11:53:21 -0700

    Added some comments to code for reference. Started on loop to listen for click 15 times.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var productNames = [];
+var productNames = []; //created by slicing off path and file type. used to define product names
 
-var allProducts = [];
+var allProducts = []; //array of Product instances
 
-var randomNumbers = [];
+var randomNumbers = []; //3 unique random numbers in range of number of images used to reference their index number in the allProducts array when rendering to DOM
 
 var imgPaths = [
   'img/bag.jpg',
@@ -33,15 +33,15 @@ var imgIDNames = [
   'leftImage',
   'middleImage',
   'rightImage'
-];
+]; //used to create document.getElementById helper function
 
 function extractNames() {
   for (var i = 0; i < imgPaths.length; i++) {
     productNames.push(imgPaths[i].slice(4, -4));
   }
-}
+} //used to slice off path and file type from image path to extract just the name
 
-extractNames();
+extractNames(); //calling function above
 
 var Product = function(productName, productPath) {
   this.name = productName;
@@ -49,15 +49,15 @@ var Product = function(productName, productPath) {
   this.numTimesShown = 0;
   this.numTimesClicked = 0;
   allProducts.push(this);
-};
+}; // constructor for Product object instances
 
 function createTheProductInstances() {
   for (var i = 0; i < imgPaths.length; i++) {
     new Product(productNames[i], imgPaths[i]);
   }
-}
+} // loop to create Product instances
 
-createTheProductInstances();
+createTheProductInstances(); // calling function above
 
 function randomImageNumber(max) {
   for (var i = 0; i < 3; i++) {
@@ -66,12 +66,11 @@ function randomImageNumber(max) {
       var number = Math.floor(Math.random() * max);
     }
     randomNumbers.push(number);
-    allProducts[number].numTimesShown +=1;
-    console.log(allProducts[number].numTimesShown);
+    allProducts[number].numTimesShown +=1; //updates value of product objects when they're shown
   }
-}
+} //used to generate unique random number array
 
-randomImageNumber(imgPaths.length);
+randomImageNumber(imgPaths.length); //calls function above
 
 function renderImage(prod, elementID) {
   var divEl = document.getElementById(elementID);
@@ -81,38 +80,42 @@ function renderImage(prod, elementID) {
   imgEl.setAttribute('class', 'randomImage')
   imgEl.setAttribute('src', prod.filePath);
   divEl.appendChild(imgEl);
-}
+} //helper function for rendering images in DOM
 
 function showImages() {
-  renderImage(allProducts[randomNumbers[0]], 'leftImage');
-  renderImage(allProducts[randomNumbers[1]], 'middleImage');
-  renderImage(allProducts[randomNumbers[2]], 'rightImage');
-}
+  renderImage(allProducts[randomNumbers[0]], imgIDNames[0]);
+  renderImage(allProducts[randomNumbers[1]], imgIDNames[1]);
+  renderImage(allProducts[randomNumbers[2]], imgIDNames[2]);
+} // uses index values of products and unique random numbers generated to render images
 
-showImages();
+showImages(); //calls function above
 
 function userClick(idName) {
   return document.getElementById(idName);
-}
+} //helper function to hook element in DOM for click event handler
 
 function imgClickEvent(event) {
   var userVote = event.target.id;
-  console.log(userVote);
   for (var i = 0; i < allProducts.length; i++) {
     if (userVote === allProducts[i].name) {
       allProducts[i].numTimesClicked += 1;
-      console.log(allProducts[i].numTimesClicked);
       randomNumbers = [ ];
       randomImageNumber(imgPaths.length);
       showImages();
     }
   }
-}
+} //update value product object when it is clicked by user
 
-function createEventListeners() {
+function createEventListeners(eventType, functionToExecute) {
   for (var i = 0; i < imgIDNames.length; i++) {
-    userClick(imgIDNames[i]).addEventListener('click', imgClickEvent);
+    userClick(imgIDNames[i]).addEventListener(eventType, functionToExecute);
   }
-}
+} // function to create event listeners on each of the three images
+createEventListeners('click', imgClickEvent);
 
-createEventListeners();
+// function eventFlow() {
+//   var counter = 0;
+//   while (counter < 15) {
+//   };
+// }
+// eventFlow(); //loop to listen for the click event 15 times
